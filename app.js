f9080351db306dff71772b74b6eb93749e511c3d
JoeNutt <joenutt01@gmail.com>
2020-08-06 13:39:46 +0100

    Added Comments to Routes folder

--- app.js
+++ app.js
@@ -22,40 +22,6 @@ app.get("/", function (req, res) {
 // COMMENTS ROUTES
 // ====================
 
-app.get("/campgrounds/:id/comments/new", function (req, res) {
-  // find campground by id
-  Campground.findById(req.params.id, function (err, campground) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("comments/new", { campground: campground });
-    }
-  });
-});
-
-app.post("/campgrounds/:id/comments", function (req, res) {
-  //lookup campground using ID
-  Campground.findById(req.params.id, function (err, campground) {
-    if (err) {
-      console.log(err);
-      res.redirect("/campgrounds");
-    } else {
-      Comment.create(req.body.comment, function (err, comment) {
-        if (err) {
-          console.log(err);
-        } else {
-          campground.comments.push(comment);
-          campground.save();
-          res.redirect("/campgrounds/" + campground._id);
-        }
-      });
-    }
-  });
-  //create new comment
-  //connect new comment to campground
-  //redirect campground show page
-});
-
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
 });
---
+++ routes/comments.js
@@ -0,0 +1,33 @@
+app.get("/campgrounds/:id/comments/new", function (req, res) {
+  // find campground by id
+  Campground.findById(req.params.id, function (err, campground) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("comments/new", { campground: campground });
+    }
+  });
+});
+
+app.post("/campgrounds/:id/comments", function (req, res) {
+  //lookup campground using ID
+  Campground.findById(req.params.id, function (err, campground) {
+    if (err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      Comment.create(req.body.comment, function (err, comment) {
+        if (err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect("/campgrounds/" + campground._id);
+        }
+      });
+    }
+  });
+  //create new comment
+  //connect new comment to campground
+  //redirect campground show page
+});
