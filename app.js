622edf833f4a694b45429f3e8c4c5f85a69c41db
JoeNutt <joenutt01@gmail.com>
2020-08-06 17:11:01 +0100

    added Login and register

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
   mongoose = require("mongoose"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
   Campground = require("./models/campground"),
   Comment = require("./models/comment.js"),
   seedDB = require("./seeds");
@@ -17,6 +19,22 @@ app.set("view engine", "ejs");
 app.get("/", function (req, res) {
   res.render("landing");
 });
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
 // PASSPORT CONFIGURATION
 app.use(
   require("express-session")({
