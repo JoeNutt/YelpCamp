8f86538b3c660c22820e95b4122a70f76c4f4008
unsivil <unsivilaudio@gmail.com>
2020-08-07 14:45:24 -0600

    more fixes, plus ui improvements, BS4

--- routes/comments.js
+++ routes/comments.js
@@ -5,44 +5,48 @@ var Comment = require("../models/comment");
 
 //Comments New
 router.get("/new", isLoggedIn, function (req, res) {
-  // find campground by id
-  console.log(req.params.id);
-  Campground.findById(req.params.id, function (err, campground) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("comments/new", { campground: campground });
-    }
-  });
+	// find campground by id
+	console.log(req.params.id);
+	Campground.findById(req.params.id, function (err, campground) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("comments/new", { campground: campground });
+		}
+	});
 });
 
 //Comments Create
 router.post("/", isLoggedIn, function (req, res) {
-  //lookup campground using ID
-  Campground.findById(req.params.id, function (err, campground) {
-    if (err) {
-      console.log(err);
-      res.redirect("/campgrounds");
-    } else {
-      Comment.create(req.body.comment, function (err, comment) {
-        if (err) {
-          console.log(err);
-        } else {
-          campground.comments.push(comment);
-          campground.save();
-          res.redirect("/campgrounds/" + campground._id);
-        }
-      });
-    }
-  });
+	//lookup campground using ID
+	Campground.findById(req.params.id, function (err, campground) {
+		if (err) {
+			console.log(err);
+			res.redirect("/campgrounds");
+		} else {
+			console.log(req.body.comment);
+			Comment.create(req.body.comment, function (err, comment) {
+				if (err) {
+					console.log(err);
+				} else {
+					comment.author.id = req.user._id;
+					comment.author.username = req.user.username;
+					comment.save();
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect("/campgrounds/" + campground._id);
+				}
+			});
+		}
+	});
 });
 
 //middleware
 function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect("/login");
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect("/login");
 }
 
 module.exports = router;
