a0bb6b872d06d215fc2c042c3087d2430c426036
JoeNutt <joenutt01@gmail.com>
2020-08-07 20:14:00 +0100

    Added author to campground model

--- models/campground.js
+++ models/campground.js
@@ -1,9 +1,16 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-var campgroundSchema = new mongoose.Schema({
+let campgroundSchema = new mongoose.Schema({
   name: String,
   image: String,
   description: String,
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    username: String,
+  },
   comments: [
     {
       type: mongoose.Schema.Types.ObjectId,
