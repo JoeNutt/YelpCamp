515093e79963f10185ccccc45961a182ab7d2058
JoeNutt <joenutt01@gmail.com>
2020-08-09 13:56:10 +0100

    added comments update and distory, and  added authorization

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var router = express.Router();
 var Campground = require("../models/campground");
-const { Route } = require("express");
+var middleware = require("../middleware");
+
 
 //INDEX - show all campgrounds
 router.get("/", function (req, res) {
@@ -11,81 +12,85 @@ router.get("/", function (req, res) {
       console.log(err);
     } else {
       res.render("campgrounds/index", {
-        campgrounds: allCampgrounds,
+        campgrounds: allCampgrounds
       });
     }
   });
 });
 
 //CREATE - add new campground to DB
-router.post("/", isLoggedIn, function (req, res) {
+router.post("/", middleware.isLoggedIn, function (req, res) {
   // get data from form and add to campgrounds array
   var name = req.body.name;
   var image = req.body.image;
+  var desc = req.body.description;
   var author = {
     id: req.user._id,
-    username: req.user.username,
-  };
-  var description = req.body.description;
-
-  var newCampground = { name, image, description, author };
+    username: req.user.username
+  }
+  var newCampground = {
+    name: name,
+    image: image,
+    description: desc,
+    author: author
+  }
   // Create a new campground and save to DB
   Campground.create(newCampground, function (err, newlyCreated) {
     if (err) {
       console.log(err);
     } else {
       //redirect back to campgrounds page
+      console.log(newlyCreated);
       res.redirect("/campgrounds");
     }
   });
 });
 
 //NEW - show form to create new campground
-router.get("/new", isLoggedIn, function (req, res) {
+router.get("/new", middleware.isLoggedIn, function (req, res) {
   res.render("campgrounds/new");
 });
 
 // SHOW - shows more info about one campground
 router.get("/:id", function (req, res) {
   //find the campground with provided ID
-  Campground.findById(req.params.id)
-    .populate("comments")
-    .exec(function (err, foundCampground) {
-      if (err) {
-        console.log(err);
-      } else {
-        //render show template with that campground
-        res.render("campgrounds/show", { campground: foundCampground });
-      }
-    });
-});
-// EDIT CAMPGROUND ROUTE
-router.get("/:id/edit", function (req, res) {
-  Campground.findById(req.params.id, function (err, foundCampground) {
+  Campground.findById(req.params.id).populate("comments").exec(function (err, foundCampground) {
     if (err) {
-      res.redirect("/campgrounds");
+      console.log(err);
     } else {
-      res.render("campgrounds/edit", { campground: foundCampground });
+      console.log(foundCampground)
+      //render show template with that campground
+      res.render("campgrounds/show", {
+        campground: foundCampground
+      });
     }
   });
 });
+
+// EDIT CAMPGROUND ROUTE
+router.get("/:id/edit", middleware.checkCampgroundOwnership, function (req, res) {
+  Campground.findById(req.params.id, function (err, foundCampground) {
+    res.render("campgrounds/edit", {
+      campground: foundCampground
+    });
+  });
+});
+
 // UPDATE CAMPGROUND ROUTE
-router.put("/:id", (req, res) => {
+router.put("/:id", middleware.checkCampgroundOwnership, function (req, res) {
   // find and update the correct campground
-  Campground.findByIdAndUpdate(req.params.id, req.body.campground, function (
-    err,
-    updatedCampground
-  ) {
+  Campground.findByIdAndUpdate(req.params.id, req.body.campground, function (err, updatedCampground) {
     if (err) {
       res.redirect("/campgrounds");
     } else {
+      //redirect somewhere(show page)
       res.redirect("/campgrounds/" + req.params.id);
     }
   });
 });
 
-// DESTORY ROUTE
-router.delete("/:id", (req, res) => {
+// DESTROY CAMPGROUND ROUTE
+router.delete("/:id", middleware.checkCampgroundOwnership, function (req, res) {
   Campground.findByIdAndRemove(req.params.id, function (err) {
     if (err) {
       res.redirect("/campgrounds");
@@ -95,11 +100,5 @@ router.delete("/:id", (req, res) => {
   });
 });
 
-function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect("/login");
-}
 
 module.exports = router;
