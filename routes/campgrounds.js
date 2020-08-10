be206a8c560f7a0b842a4cfba22b2151809b6c5a
JoeNutt <joenutt01@gmail.com>
2020-08-10 18:48:17 +0100

    navbar bug

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -12,7 +12,8 @@ router.get("/", function (req, res) {
       console.log(err);
     } else {
       res.render("campgrounds/index", {
-        campgrounds: allCampgrounds
+        campgrounds: allCampgrounds,
+        page: 'campgrounds'
       });
     }
   });
